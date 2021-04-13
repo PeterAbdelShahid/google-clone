6dda1807ecbec404bb065c29874e55e8f3bd7325
Peter AbdelShahid <peter.essam@gmail.com>
2021-04-13 09:45:18 +0200

    nextlink

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import Link from "next/Link";
+import NextLink from "next/Link";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 
 function PaginationButtons() {
@@ -9,21 +9,23 @@ function PaginationButtons() {
   return (
     <div className="flex justify-between max-w-lg text-blue-700 mb-10 ">
       {startIndex >= 10 && (
-        <Link
+        <NextLink
           href={`/search?term=${router.query.term}&start=${startIndex - 10}`}
         >
           <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
             <ChevronLeftIcon className="h-5" />
             <p>Previous</p>
           </div>
-        </Link>
+        </NextLink>
       )}
-      <Link href={`/search?term=${router.query.term}&start=${startIndex + 10}`}>
+      <NextLink
+        href={`/search?term=${router.query.term}&start=${startIndex + 10}`}
+      >
         <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
           <ChevronRightIcon className="h-5" />
           <p>Next</p>
         </div>
-      </Link>
+      </NextLink>
     </div>
   );
 }
